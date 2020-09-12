96cf4d016aff5eef8d1d30332a5847969ad10f9c
lizhi <zhi.li@wz-inc.com>
2020-09-12 19:13:57 +0800

    format code

--- src/factory/index.js
+++ src/factory/index.js
@@ -1,19 +1,16 @@
 class ConnentFactory {
-
-    static create(type) {
-      if (type === 'tcp')
-        return new Connent(type, 108000, 300);
-      if (type === 'upd')
-        return new Connent(type, 111000, 320);
-    }
+  static create(type) {
+    if (type === "tcp") return new Connent(type, 108000, 300);
+    if (type === "upd") return new Connent(type, 111000, 320);
   }
+}
 
-  class Connent {
-    constructor(model, ip, port) {
-      this.model = model;
-      this.ip = ip;
-      this.port = port;
-    }
+class Connent {
+  constructor(model, ip, port) {
+    this.model = model;
+    this.ip = ip;
+    this.port = port;
   }
-  
-  export default ConnentFactory;
+}
+
+export default ConnentFactory;
